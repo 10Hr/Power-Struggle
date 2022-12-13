b9abe8344eb0e0de57d69107396e2895ed8fb51f
johncderstine <johncderstine@gmail.com>
2022-12-13 14:46:40 -0500

    Cleaned up PlayerScript

--- Assets/Scripts/Player/PlayerScript.cs
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -39,7 +39,6 @@ public class PlayerScript : NetworkBehaviour
 
     public void Start()
     {
-        //CmdGetScripts();
         playerList = GameObject.Find("PlayerList").GetComponent<PlayerList>();
         FSM = GameObject.Find("FSM").GetComponent<GameState>();
         CmdSetPlayer(this);
@@ -47,13 +46,6 @@ public class PlayerScript : NetworkBehaviour
 
     public void Update()
     {
-
-        //if (isServer && playerList.players.Count == 4)
-        //{
-        //    CmdSendPlayers(playerList.players);
-        //    Debug.Log("It shoudl be working");
-        //}
-
         if (GameObject.Find("13") != null && !added && isServer)
         {
             CmdSendPlayers(this);
@@ -83,8 +75,6 @@ public class PlayerScript : NetworkBehaviour
     public void CmdSetPlayer(PlayerScript player)
     {
         RpcSetPlayer();
-        //if (!playerList.players.Contains(player))
-        //playerList.CmdAddPlayers(player);
     }
 
     [ClientRpc]
@@ -93,52 +83,4 @@ public class PlayerScript : NetworkBehaviour
         playerNumber = (int)netIdentity.netId;
         gameObject.name = $"{playerNumber}";
     }
-
-    //[Command(requiresAuthority = false)]
-    //public void CmdSendPlayers(List<PlayerScript> players)
-    //{
-    //    RpcRecievePlayers(players);
-    //}
-
-    //[ClientRpc]
-    //public void RpcRecievePlayers(List<PlayerScript> players)
-    //{
-    //    this.playerList.players = players;
-    //}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //[Command(requiresAuthority = false)]
-    //public void CmdGetScripts()
-    //{
-    //    Debug.Log("getting scripts");
-    //    if (!isServer)
-    //        RpcGetScripts();
-    //}
-
-    //[ClientRpc]
-    //public void RpcGetScripts()
-    //{
-    //    if
-    //    FSM = GameObject.Find("FSM").GetComponent<GameState>();
-    //    Debug.Log(FSM + " fsm");
-    //    playerList = GameObject.Find("PlayerList").GetComponent<PlayerList>();
-    //}
 }
